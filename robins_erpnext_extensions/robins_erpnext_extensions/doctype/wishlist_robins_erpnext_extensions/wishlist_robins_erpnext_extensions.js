0b59feeb922f3ee49b28c246633f61bf14d68d72
Robin Rosenstock <robin.rosenstock@t-online.de>
2021-01-23 09:31:38 +0100

    moved wishlist from custom script to robinserpnextextensions

--- robins_erpnext_extensions/robins_erpnext_extensions/doctype/wishlist_robins_erpnext_extensions/wishlist_robins_erpnext_extensions.js
+++ robins_erpnext_extensions/robins_erpnext_extensions/doctype/wishlist_robins_erpnext_extensions/wishlist_robins_erpnext_extensions.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2020, Robin Rosenstock and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Wishlist_robins_erpnext_extensions', {
-	// refresh: function(frm) {
 
-	// }
-});
+frappe.ui.form.on('Wishlist_robins_erpnext_extensions', {
+	onload(frm) {
+			frappe.call({
+			method: "frappe.desk.form.linked_with.get_linked_doctypes",
+			args: {
+			    'doctype': 'Wishlist_robins_erpnext_extensions'
+			},
+			callback: function(r) {
+				console.log(r)
+				
+							frappe.call({
+			method: "frappe.desk.form.linked_with.get_linked_docs",
+			args: {
+			    'doctype': 'Wishlist_robins_erpnext_extensions',
+			    'name': 'Future-Tech',
+			    'linkinfo': r.message
+			},
+			callback: function(r2) {
+				console.log(r2)
+			}
+			})
+			}
+			})
+	}
+})
